7a78e4cd3c3cd2eef1f22db536b688021f4cf9d1
jaeno198 <jacksonmateus96@gmail.com>
2026-06-12 00:15:29 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,16 @@ export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: { '@': fileURLToPath(new URL('./src', import.meta.url)) }
+  },
+  server: {
+    port: 5173,
+    proxy: {
+      // Redireciona /api/* para o backend FastAPI
+      '/api': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      }
+    }
   }
 })
